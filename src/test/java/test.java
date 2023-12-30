39b9f7cf092c07f22d89ef50b3a96345270d5f4b
huykhongcan04 <huykhongcan04@gmail.com>
2023-12-30 23:14:41 +0700

    none

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,13 +3,31 @@ import com.courseoutlinemanager.common.ProcessString;
 import java.util.ArrayList;
 
 public class test {
-    public static void main(String[] args) {
-        String a = "H  u . _y";
-        String b = "        h  u   -_ y          ";
+    private static final int WIDTH = 50;
+    private static final int INDENT_SPACE = 4;
+
+    public static void printLabel(String label, String character) {
+        int halfLineLength = (WIDTH - label.length()) / 2;
+        String horizontialLine = character.repeat(halfLineLength);
+        System.out.println(horizontialLine + label + horizontialLine);
     }
-    public static void change(String s1, String s2){
-        s1= s1.toUpperCase();
-        s2= s2.toUpperCase();
-        System.out.println(s1+s2);
+
+    public static void printLine(String text, int indent) {
+        int remainLength = WIDTH - 2 - text.length() - indent * INDENT_SPACE;
+        String indentString = " ".repeat(INDENT_SPACE * indent);
+        System.out.println("|"+ indentString + text + " ".repeat(remainLength)+"|");
+    }
+
+    public static void main(String[] args) {
+        printLabel("De cuong mon hoc", "=");
+        printLine("Course name: OOP",0);
+        printLine("Course code:  ITEC2504",0);
+        printLine("Mon hoc truoc: ",0);
+        printLine("Ky thuat lap trinh",1);
+        printLine("Ky thuat lap trinh",2);
+        printLine("Ky thuat lap trinh",3);
+        printLine("Ky thuat lap trinh",0);
+        printLabel("","=");
+
     }
 }
